670e521897b2737aa4a01727ad4b5f4060ddd773
Tsvil12 <pachacha123a@gmail.com>
2026-06-02 23:35:18 +0700

    реализован весь функционал

--- src/main/kotlin/com/example/springfirst/SpringFirstApplication.kt
+++ src/main/kotlin/com/example/springfirst/SpringFirstApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringFirstApplication
+open class SpringFirstApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringFirstApplication>(*args)
